dc062598da8b9be57065b824314241415cfb2252
Marco Guidi <mark.88g@gmail.com>
2020-10-21 14:44:50 +0200

    creata funzione di comparazione

--- js-1 palindromo/assets/main.js
+++ js-1 palindromo/assets/main.js
@@ -16,6 +16,12 @@ userWordReverse = stringRevesator(userWord);
 // check
 console.log("Parola al contrario: " + userWordReverse);
 
+// CONFRONT USER-WORD WITH THE REVERSE
+if (areSameString(userWord, userWordReverse)) {
+    console.log("La parola inserita è palindroma");
+} else {
+    console.log("La parola inserita NON è palindroma");
+}
 
 
 
@@ -24,11 +30,11 @@ console.log("Parola al contrario: " + userWordReverse);
 // </1-fold END --- PALINDROME VERSION 1.0
 
 // <1-fold START --- ALL FUNCTION
+// FOR REVERSE A STRING
 function stringRevesator(string) {
-    var string = string;
-    // check
-    console.log("*** Funzione stringReversator output ***");
-    console.log(string + " di lunghezza " + string.length);
+    // // check
+    // console.log("*** Funzione stringReversator output ***");
+    // console.log(string + " di lunghezza " + string.length);
 
     // STRING REVERSE GENERATOR
     var stringReverseArray = [];
@@ -40,19 +46,60 @@ function stringRevesator(string) {
         stringReverseArray.push(string[i]);
 
     }
-    // check
-    console.log(stringReverseArray);
+    // // check
+    // console.log(stringReverseArray);
 
     // from array to string
     for (var i = 0; i < stringReverseArray.length; i++) {
         stringReverse += stringReverseArray[i];
     }
-    // check
-    console.log(stringReverse);
-    console.log("*** Funzione stringReversator output END ***");
+    // // check
+    // console.log(stringReverse);
+    // console.log("*** Funzione stringReversator output END ***");
 
     // *** result ***
     return stringReverse;
+}
+
+// FOR CONFRONT 2 STRING --> "true" if same string
+function areSameString(string1, string2) {
+    // check
+    console.log("Sto comparando " + string1 + " con " + string2);
+
+    // VAR ASSIGNMENT
+    var stringCompareControl = true;
+
+    // first check: string-length-compare
+    if (string1.length == string2.length) {
+        // same-length-string
+
+        // // check
+        // console.log("hanno stessa lunghezza");
+
+        // cycle for compare letter with same index
+        for (var i = 0; i < string1.length; i++) {
+            // IF one letter is different
+            if (string1[i] != string2[i]) {
+                stringCompareControl = false;
+
+                // // check
+                // console.log(i + "esima lettera diversa");
+            } else {
+                // // check
+                // console.log(i + "esima lettera uguale");
+            }
+        }
+    } else {
+        // not-same-length-string
+        stringCompareControl = false;
+
+        // // check
+        // console.log("Non hanno stessa lunghezza");
+    }
+
+    // check
+    console.log(stringCompareControl);
+    return stringCompareControl;
 }
 
 // </1-fold END --- ALL FUNCTION
