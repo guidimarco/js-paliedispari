bca20a8fe49347a91f8906f292a54dc45cf6aa7e
Marco Guidi <mark.88g@gmail.com>
2020-10-21 13:14:43 +0200

    file vuoti

---
+++ js-1 palindromo/assets/main.js
@@ -0,0 +1,15 @@
+
+// <1-fold START --- PALINDROME VERSION 1.0
+console.log("Palindrome Version 1.0:");
+
+// ASK A WORD
+userWord = prompt("Inserisci una parola").toLowerCase().trim();
+// check
+console.log(userWord);
+
+
+
+
+
+
+// </1-fold END --- FIZZ-BUZZ ALGORITHM VERSION 1.0
---
+++ js-2 pari dispari/assets/main.js
@@ -0,0 +1,15 @@
+
+// <1-fold START --- EVEN OR ODD VERSION 1.0
+// console.log("Palindrome Version 1.0:");
+//
+// // ASK A WORD
+// userWord = prompt("Inserisci una parola").toLowerCase().trim();
+// // check
+// console.log(userWord);
+
+
+
+
+
+
+// </1-fold END --- EVEN OR ODD VERSION 1.0
